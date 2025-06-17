e88464cca0c3ccd1504e25a67d83a231a59760ae
Mike Will <Mike.Rego@baymetrics.com.br>
2025-06-17 19:29:48 -0300

    feat: route not found screen

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Buildings from "./front-end/pages/buildings";
+import NotFound from "./front-end/pages/not-found";
 
 export default function Root(props) {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/buildings" element={<Buildings />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
